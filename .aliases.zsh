0bc0c5820b6016473dac7a932a435fdb49113419
Rory Grieve <rhrg500@gmail.com>
2018-01-30 17:05:04 +0000

    Add database shortcuts
    Using functions is fun

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,3 +1,14 @@
 alias be='bundle exec'
 alias clone='bundle exec rake clone_remote_db:production'
 alias fs='foreman start'
+
+reset_test_db() {
+  RAILS_ENV=test be rake db:drop
+  RAILS_ENV=test be rake db:create
+  RAILS_ENV=test be rake db:migrate
+}
+
+clone_migrate() {
+  bundle exec rake clone_remote_db:production
+  bundle exec rake db:migrate
+}
