18d3365776bf82cdbe90f9a64456c2ac242c8682
Rory Grieve <rhrg500@gmail.com>
2018-02-02 11:42:40 +0000

    Echo explanations in functions

--- .aliases.zsh
+++ .aliases.zsh
@@ -3,12 +3,16 @@ alias clone='bundle exec rake clone_remote_db:production'
 alias fs='foreman start'
 
 function reset_test_db() {
+  echo Dropping test database
   RAILS_ENV=test be rake db:drop
+  echo Creating test database
   RAILS_ENV=test be rake db:create
+  echo Applying migrations to test database
   RAILS_ENV=test be rake db:migrate
 }
 
 function clone_migrate() {
   bundle exec rake clone_remote_db:production
+  echo Applying migrations to development database
   bundle exec rake db:migrate
 }
